627ade4e22a234f38f7f5718f1c873322f98cd69
Velen Aranha <t-velen.aranha@paypay-corp.co.jp>
2022-01-27 16:41:29 +0530

    Apply linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
+  selector: 'Root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shoppy';
+  title = 'shoppy'
 }
